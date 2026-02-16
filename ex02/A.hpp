5c8b76800b44e982ed88e6399f806a95925fe3c4
kapinarc <89167555+KadalK@users.noreply.github.com>
2026-02-16 18:14:03 +0100

    implement class for ex02

---
+++ ex02/A.hpp
@@ -0,0 +1,11 @@
+#ifdef A_HPP
+#define A_HPP
+#include "Base.hpp"
+
+class A : public Base{
+public:
+	A();
+	~A();
+};
+
+#endif
---
+++ ex02/Base.hpp
@@ -0,0 +1,12 @@
+#ifdef BASE_HPP
+#define BASE_HPP
+
+
+class Base {
+public:
+	Base();
+	void identify(Base* p);
+	virtual ~Base();
+};
+
+#endif
